67e910637b3b8db7815196efb4af4f9d2e11debf
Allan Quiros <allan.quiros@outlook.es>
2020-07-12 20:28:15 -0600

    branch 01-RepasoJs

---
+++ 01-RepasoJs/app.js
@@ -0,0 +1,11 @@
+// Crear variable con var
+var aprendiendoJS = true;
+console.log(aprendiendoJS);
+
+// Crear variable con const
+const aprendiendo = "Javascript";
+
+// Crear variable con let
+let aprendiendoLet;
+
+
---
+++ 01-RepasoJs/scope.js
@@ -0,0 +1,19 @@
+// Scope
+
+// var music = "Rock";
+
+// if(music){
+//     var music = "Grunge";
+//     console.log("Dentro del if ",music);
+// }
+// console.log("Fuera del if ",music);
+
+// Scope con let
+ let music = "Rock";
+
+ if(music){
+     var music = "Grunge";
+     console.log("Dentro del if ",music);
+ }
+ console.log("Fuera del if ",music);
+

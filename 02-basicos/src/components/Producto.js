306edbaa36259dec08ddb3f6d97b033e64f6576f
Allan Quiros <allan.quiros@outlook.es>
2020-07-12 20:41:30 -0600

    02-basicos

---
+++ 02-basicos/src/components/Producto.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Producto = ({ producto, carrito, agregarProducto, productos }) => {
+  
+  // Agregar producto al carrito
+  const seleccionarProducto = (id) => {
+    const producto = productos.filter(producto => producto.id === id)[0];
+    agregarProducto(
+      [
+        ...carrito,
+        producto
+      ]
+      );
+    };
+    const { nombre, precio, id } = producto;
+  
+  return (
+    <div>
+      <h2>{nombre}</h2>
+      <p>${precio}</p>
+      <button 
+      type="button"
+      onClick= { () => seleccionarProducto(id) }
+      >Comprar</button>
+    </div>
+  );
+};
+
+export default Producto;

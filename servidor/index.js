566c5210a888568f4d05fc0f7f99acc7a5ff79d2
Allan Quiros <allan.quiros@outlook.es>
2020-08-23 18:55:35 -0600

    MongoDb configuration

--- servidor/index.js
+++ servidor/index.js
@@ -1,1 +1,19 @@
-console.log("Desde index.jsre");
+// Importar el servidor
+const express = require('express');
+// Importamos la conexion de la bd del archivo Config/db.js
+const connectDB = require('./config/db');
+
+// Creamos el servidor
+const app = express();
+
+// Conectar a la base de datos
+connectDB();
+
+// Usamos el puerto del archivo env o por defecto el 4000
+// puerto del app
+const PORT = process.env.PORT || 4000;
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`El servidor se ejecuta en el puerto ${PORT}`);
+})
